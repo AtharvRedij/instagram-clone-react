5add40008976f72d01dfd95bb1ac17e0fcef2557
AtharvRedij <atharvaredij@gmail.com>
2020-07-31 20:00:13 +0530

    Updates

---
+++ precache-manifest.f1c5684e87e5fcd0ab7bec5a09b3cab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbab7554408803bbf9889298c36461e",
+    "url": "/instagram-clone-react/index.html"
+  },
+  {
+    "revision": "26dd4acfb67dfe99ecf4",
+    "url": "/instagram-clone-react/static/css/main.ce3fbcfa.chunk.css"
+  },
+  {
+    "revision": "c8a9b5580534ca4131e6",
+    "url": "/instagram-clone-react/static/js/2.8763e269.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/instagram-clone-react/static/js/2.8763e269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26dd4acfb67dfe99ecf4",
+    "url": "/instagram-clone-react/static/js/main.ee0c0757.chunk.js"
+  },
+  {
+    "revision": "497fc0ea82d334bee223",
+    "url": "/instagram-clone-react/static/js/runtime-main.6e3921e1.js"
+  }
+]);
